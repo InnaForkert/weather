3e13e436139ec522aeb9581c452db19b95aa7abd
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-28 00:57:29 +0200

    add: geocoding api

--- src/redux/utils/getWeather.ts
+++ src/redux/utils/getWeather.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://api.openweathermap.org/data/2.5/weather";
-
-const key = "5594fc1d85d0401b513dc15c2ba32cba";
+const key = "4d63b34e2bfdbbcbd4d5a5f459e43328";
 
 export const fetchWeather = createAsyncThunk(
   "weather/fetch",
   async (city: string, thunkAPI) => {
     try {
-      const response = await axios.get(`?q=${city}&appid=${key}`);
+      const cityCoordsObj: { lat: number; lon: number }[] = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}}`
+      );
+      const cityLat = cityCoordsObj[0].lat;
+      const cityLon = cityCoordsObj[0].lon;
+
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${key}`
+      );
       return response.data;
     } catch (e) {
       if (e instanceof Error) {
