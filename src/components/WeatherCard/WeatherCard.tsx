12984b804ae8ddf7ada3341bc39a99df6da1f718
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-29 04:33:37 +0200

    refresh btn

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,24 +1,32 @@
-import { Card, Typography } from "@mui/material";
+import { Button, Card, Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import CachedIcon from "@mui/icons-material/Cached";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useAppDispatch, useAppSelector } from "../../redux/utils/hooks";
 import { useNavigate } from "react-router";
 import { fetchWeather } from "../../redux/utils/getWeather";
+import { useState } from "react";
 
 export function WeatherCard({ cityName }: { cityName: string }) {
+  const isLoading = useAppSelector((state) => state.weather.isLoading);
   const values = useAppSelector((state) => state.weather.values);
   const weather = values.find((el) => el.name === cityName);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [id, setId] = useState("");
 
-  async function handleOnClick(e: React.MouseEvent) {
+  function handleCardClick(e: React.MouseEvent) {
     const target = e.target as HTMLElement;
-    if (target.nodeName === "svg") {
-      dispatch(fetchWeather(cityName));
-      console.log("hi");
-    } else {
+    if (target.nodeName !== "button") {
       navigate(`details/${cityName}`);
     }
+  }
+
+  async function refreshWeather() {
+    setId(cityName);
+    await dispatch(fetchWeather(cityName));
+    setId("");
   }
 
   let imgUrl = "";
@@ -30,27 +38,37 @@ export function WeatherCard({ cityName }: { cityName: string }) {
   }
 
   return (
-    <Card onClick={handleOnClick}>
+    <Card>
       <Grid2
+        onClick={handleCardClick}
         container
         alignItems="center"
         spacing={2}
-        justifyContent="space-around"
+        flexDirection="column"
       >
-        <Grid2>
+        <Grid2 container alignItems="center">
           <img src={imgUrl} alt="" />
-        </Grid2>
-        <Grid2>
           <Typography variant="h4" component="p">
             {temp}°C
           </Typography>
         </Grid2>
-      </Grid2>
-      <Grid2 container justifyContent="center">
-        <CachedIcon fontSize="large" />
-        <Typography variant="h5" component="p" textAlign="center" pb={3} ml={2}>
+        <Typography variant="h5" component="p" textAlign="center" ml={2}>
           {cityName}
         </Typography>
+      </Grid2>
+      <Grid2 container justifyContent="space-around">
+        <LoadingButton
+          loading={isLoading && id === cityName}
+          loadingIndicator="Loading…"
+          variant="contained"
+          startIcon={<CachedIcon />}
+          onClick={refreshWeather}
+        >
+          Refresh
+        </LoadingButton>
+        <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
       </Grid2>
     </Card>
   );
