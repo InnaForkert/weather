66b0ac76a188916f69413f321e180ed91d0c0282
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-29 04:58:29 +0200

    back to main page btn

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,9 +1,14 @@
-import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+
+import { useNavigate, useParams } from "react-router-dom";
+
 import { useAppDispatch, useAppSelector } from "../redux/utils/hooks";
 import { fetchHourlyForecast } from "../redux/utils/getWeather";
-import { Card, Typography } from "@mui/material";
+
+import { Button, Card, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+
 import { HourlyChart } from "../components/HourlyChart/HourlyChart";
 
 export function Details() {
@@ -11,6 +16,7 @@ export function Details() {
   const weather = useAppSelector((state) => state.weather.values);
   const hourlyWeather = useAppSelector((state) => state.weather.currentWeather);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const currentCityWeather = weather.find((el) => el.name === cityId);
 
@@ -27,6 +33,7 @@ export function Details() {
   let pressure = 0;
   let description = "";
   let windSpeed = 0;
+  let cityName = cityId;
 
   if (currentCityWeather) {
     imgUrl = `http://openweathermap.org/img/wn/${currentCityWeather.weather[0].icon}@2x.png`;
@@ -36,35 +43,49 @@ export function Details() {
     pressure = Math.round(currentCityWeather.main.pressure);
     description = currentCityWeather.weather[0].description;
     windSpeed = Math.round(currentCityWeather.wind.speed);
+    cityName =
+      currentCityWeather.name[0].toUpperCase() +
+      currentCityWeather.name.slice(1).toLowerCase();
   }
 
   return (
-    <Card>
-      <Grid2 container alignItems="center">
-        <Grid2 sm={12} md={5}>
-          <Grid2 container alignItems="center">
-            <Typography variant="h3" component="h1" mb={2}>
-              {cityId}
+    <>
+      <Card>
+        <Grid2 container alignItems="center">
+          <Grid2 sm={12} md={5}>
+            <Grid2 container alignItems="center">
+              <Typography variant="h3" component="h1" mb={2}>
+                {cityName}
+              </Typography>
+              <img src={imgUrl} alt="" />
+            </Grid2>
+            <Typography variant="h5" component="p" mb={1}>
+              Temperature: {temp}°C
             </Typography>
-            <img src={imgUrl} alt="" />
+            <Typography variant="h5" component="p" mb={1}>
+              Feels like: {feelsLike}°C
+            </Typography>
+            <Typography fontStyle="italic" mb={2}>
+              {description}
+            </Typography>
+            <Typography>Humidity: {humidity}%</Typography>
+            <Typography>Pressure: {pressure} mb</Typography>
+            <Typography>Wind speed: {windSpeed} m/s</Typography>
           </Grid2>
-          <Typography variant="h5" component="p" mb={1}>
-            Temperature: {temp}°C
-          </Typography>
-          <Typography variant="h5" component="p" mb={1}>
-            Feels like: {feelsLike}°C
-          </Typography>
-          <Typography fontStyle="italic" mb={2}>
-            {description}
-          </Typography>
-          <Typography>Humidity: {humidity}%</Typography>
-          <Typography>Pressure: {pressure} mb</Typography>
-          <Typography>Wind speed: {windSpeed} m/s</Typography>
+          <Grid2 sm={11} md={7}>
+            <HourlyChart weather={hourlyWeather} />
+          </Grid2>
+          <Grid2>
+            <Button
+              variant="contained"
+              startIcon={<ArrowCircleLeftIcon />}
+              onClick={() => navigate("/")}
+            >
+              Back to all cities
+            </Button>
+          </Grid2>
         </Grid2>
-        <Grid2 sm={11} md={7}>
-          <HourlyChart weather={hourlyWeather} />
-        </Grid2>
-      </Grid2>
-    </Card>
+      </Card>
+    </>
   );
 }
